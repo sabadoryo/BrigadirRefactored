288a403e4d2abddd2520d1007773b20d59525c3e
sabadoryo <eabilbay@gmail.com>
2022-02-20 16:55:28 +0600

    Dzhuko! Made some refactoring take a look for var types and change export statements, if possible

--- app/listeners/messageCreate.js
+++ app/listeners/messageCreate.js
@@ -1,12 +1,11 @@
+const CommandProcessor = require('../components/commandProcessor');
+  
 module.exports = async msg => {
   if (msg.author.bot) return;
 
   if (msg.content.startsWith(process.env.COMMAND_SUFFIX)) {
-
-    const CommandProcessor = require('../components/commandProcessor');
     const commandProcessor = new CommandProcessor(msg);
     commandProcessor.start();
-
   } else {
     console.log("Not command")
   }
