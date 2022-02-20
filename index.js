e3f6c3c75de844924213a98a60d987835060617a
sabadoryo <eabilbay@gmail.com>
2022-02-20 18:14:05 +0600

    Some refactoring on index.js

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ require('dotenv').config()
 const App = require('./app/app')
 const ready = require('./app/listeners/ready/ready')
 const messageCreate = require('./app/listeners/messageCreate/messageCreate')
-// const {PrismaClient} = require('@prisma/client')
-// const prisma = new PrismaClient()
-const app = new App(process.env.TOKEN)
 
+const app = new App(process.env.TOKEN)
 const client = app.start()
 
 client.on('ready', ready);
