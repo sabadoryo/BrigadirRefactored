5330a992a5acb82bb6f60239325aea8c585b759d
sabadoryo <eabilbay@gmail.com>
2022-03-14 04:03:23 +0600

    roll with luck

--- app/commands/roll.js
+++ app/commands/roll.js
@@ -4,6 +4,11 @@ class Roll {
   }
 
   async run(user, discordMessage, commandsName, params) {
+
+    if (params.includes('--with-luck')) {
+      discordMessage.reply('100')
+      return;
+    }
     const randomNumber = await Math.floor(Math.random() * 100)
     discordMessage.reply(`${randomNumber}`)
   }
