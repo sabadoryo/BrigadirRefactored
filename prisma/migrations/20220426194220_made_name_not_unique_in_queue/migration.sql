7acfa1c67392a6eacab51f5d797c5cb780d4f4c7
sabadoryo <eabilbay@gmail.com>
2022-04-27 03:53:35 +0600

    fixed migration

--- prisma/migrations/20220426194220_made_name_not_unique_in_queue/migration.sql
+++ prisma/migrations/20220426194220_made_name_not_unique_in_queue/migration.sql
@@ -2,5 +2,4 @@
 DROP INDEX `Queue_name_key` ON `Queue`;
 
 -- AlterTable
-ALTER TABLE `Queue` ADD COLUMN `created_at` DATETIME(3) NULL DEFAULT CURRENT_TIMESTAMP(3),
-    MODIFY `name` VARCHAR(191) NULL DEFAULT 'null';
+ALTER TABLE `Queue` MODIFY `name` VARCHAR(191) NULL DEFAULT 'null';
